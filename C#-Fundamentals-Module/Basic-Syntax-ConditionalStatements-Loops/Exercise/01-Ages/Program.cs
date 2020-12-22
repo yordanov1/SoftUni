af05925632a0d01e47cf6d199d1902834e87d132
yordanov1 <yordanov8801@gmail.com>
2020-12-22 19:11:34 +0200

    Add tasks

---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Exercise/01-Ages/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EXER_01.Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ages = int.Parse(Console.ReadLine());
+
+            if(ages >= 0 && ages <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if(ages >= 3 && ages <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (ages >= 14 && ages <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (ages >= 20 && ages <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (ages >= 20 && ages <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (ages >= 66)
+            {
+                Console.WriteLine("elder");
+            }
+
+        }
+    }
+}
+
---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Exercise/03-Vacation/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace _03_Жацантион
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            string tipeOfGroup = Console.ReadLine();
+            string day = Console.ReadLine();
+
+            double totalPrice = 0;
+            if(tipeOfGroup == "Students")
+            {
+                if (day == "Friday")
+                {
+                    totalPrice = numberOfPeople * 8.45;
+
+
+                }
+                else if (day == "Saturday")
+                {
+                    totalPrice = numberOfPeople * 9.80;
+                }
+                else if (day == "Sunday")
+                {
+                    totalPrice = numberOfPeople * 10.46;
+                }
+
+                
+
+                if(numberOfPeople >= 30)
+                {
+                    totalPrice *= 0.85;
+                }
+
+            }
+            else if( tipeOfGroup == "Business")
+            {
+                if(numberOfPeople >= 100)
+                {
+                    numberOfPeople -= 10;
+                }
+
+                if ( day == "Friday")
+                {
+                    totalPrice = numberOfPeople * 10.90;
+                }
+                else if ( day == "Saturday")
+                {
+                    totalPrice = numberOfPeople * 15.60;
+                }
+                else if ( day == "Sunday")
+                {
+                    totalPrice = numberOfPeople * 16;
+                }
+            }
+            else if (tipeOfGroup == "Regular")
+            {
+                if(day == "Friday")
+                {
+                    totalPrice = numberOfPeople * 15;
+                }
+                else if (day == "Saturday")
+                {
+                    totalPrice = numberOfPeople * 20;
+                }
+                else if (day == "Sunday")
+                {
+                    totalPrice = numberOfPeople * 22.50;
+                }
+
+                if(numberOfPeople >= 10 && numberOfPeople <= 20)
+                {
+                    totalPrice *= 0.95;
+
+                }
+            }
+            Console.WriteLine($"Total price: {totalPrice:f2}");
+        }
+    }
+}
