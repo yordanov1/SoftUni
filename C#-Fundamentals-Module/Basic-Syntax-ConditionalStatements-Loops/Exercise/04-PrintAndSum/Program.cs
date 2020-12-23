49cd11d854bcea9447dd74daf24f17ee787792e9
yordanov1 <yordanov8801@gmail.com>
2020-12-23 21:13:42 +0200

    Add tasks

---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Exercise/04-PrintAndSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
