91680ab50384daee2ba6d4f41ee3c0434623b26e
yordanov1 <yordanov8801@gmail.com>
2020-12-24 21:09:03 +0200

    Add tasks

---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Exercise/05-Login/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.LoginEXER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+            int count = 0;
+
+            while (true)
+            {
+                string currentUser = Console.ReadLine();
+
+                if ( currentUser != password)
+                {
+                    count++;
+
+                    if(count == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+            }
+        }
+    }
+}
