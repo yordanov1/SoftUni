74d922cd1e237b32eed5b26cdefa45fe63dc58a9
yordanov1 <yordanov8801@gmail.com>
2020-12-28 20:43:34 +0200

    Add task

---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Exercise/08-TriangleOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08.TriangleOfNumbersEXER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
