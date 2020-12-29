16b73cf3280d64f0521932c9ceb6442f379a46b9
yordanov1 <yordanov8801@gmail.com>
2020-12-29 12:54:40 +0200

    Add task

---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Exercise/09-PadawanEquipment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EXER_09.PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceLightsaber = double.Parse(Console.ReadLine());
+            double priceRobe = double.Parse(Console.ReadLine());
+            double priceBelt = double.Parse(Console.ReadLine());
+
+
+            double countSabres = Math.Ceiling(students * 1.1);
+            int freeBelts = students / 6;
+
+            double lightsaberAll = countSabres * priceLightsaber;
+            double robesAll = students * priceRobe;
+            double beltsAll = (students - freeBelts) * priceBelt;
+
+            double all = lightsaberAll + robesAll + beltsAll;
+
+            if(money >= all)
+            {
+                Console.WriteLine($"The money is enough - it would cost {all:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {(all - money):f2}lv more.");
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Exercise/10-Rage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10.RageEXER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+
+            double money = headsetPrice * (lostGames / 2);
+            money += mousePrice * (lostGames / 3);
+            money += keyboardPrice * (lostGames / 6);
+            money += displayPrice * (lostGames / 12);
+
+            Console.WriteLine($"Rage expenses: {money:f2} lv.");
+
+
+
+
+        }
+    }
+}
