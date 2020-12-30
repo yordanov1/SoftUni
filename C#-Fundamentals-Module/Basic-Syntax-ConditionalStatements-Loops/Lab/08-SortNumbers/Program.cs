53d092c602750b35a89c903ff38adde745835b0d
yordanov1 <yordanov8801@gmail.com>
2020-12-30 17:55:30 +0200

    Add task

---
+++ C#-Fundamentals-Module/Basic-Syntax-ConditionalStatements-Loops/Lab/08-SortNumbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MORE_01.SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+            double third = double.Parse(Console.ReadLine());
+
+
+            if(first > second && first > third)
+            {
+                Console.WriteLine(first);
+             if(second > third)
+                {
+                    Console.WriteLine(second);
+                    Console.WriteLine(third);
+                }
+                else
+                {
+                    Console.WriteLine(third);
+                    Console.WriteLine(second);
+                }
+            }
+            else if(second > first && second > third)
+            {
+                Console.WriteLine(second);
+                if(first > third)
+                {
+                    Console.WriteLine(first);
+                    Console.WriteLine(third);
+                }
+                else
+                {
+                    Console.WriteLine(third);
+                    Console.WriteLine(first);
+                }
+            }
+            else if(third > first && third > second)
+            {
+                Console.WriteLine(third);
+                if(first > second)
+                {
+                    Console.WriteLine(first);
+                    Console.WriteLine(second);
+                }
+                else
+                {
+                    Console.WriteLine(second);
+                    Console.WriteLine(first);
+                }
+            }
+        }
+    }
+}
