c2d29002f47905a786bef4ba2fcb3f98604a8e23
yordanov1 <yordanov8801@gmail.com>
2021-01-05 12:58:52 +0200

    Add Data types

---
+++ C#-Fundamentals-Module/Data-Types-And-Variables/Exercise/11-SnowBall/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace EXER_11.SnowBall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger bestValue = 0;
+            int bestSnow = 0;
+            int bestTime = 0;
+            int bestQuality = 0;
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                int ballSnow = int.Parse(Console.ReadLine());
+                int ballTime = int.Parse(Console.ReadLine());
+                int ballQuality = int.Parse(Console.ReadLine());
+
+                int ballSnowTime = ballSnow / ballTime;
+                BigInteger ballValue = BigInteger.Pow(ballSnowTime,ballQuality);
+
+                if(bestValue < ballValue)
+                {
+                    bestValue = ballValue;
+                    bestSnow = ballSnow;
+                    bestTime = ballTime;
+                    bestQuality = ballQuality;
+                }
+
+            }
+
+            Console.WriteLine($"{bestSnow} : {bestTime} = {bestValue} ({bestQuality})");
+        }
+    }
+}
