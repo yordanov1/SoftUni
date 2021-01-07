4b4a2bde38d8a7bc9f0f215584aa6e82575e1bb2
yordanov1 <yordanov8801@gmail.com>
2021-01-07 19:23:53 +0200

    Add Types

---
+++ C#-Fundamentals-Module/Data-Types-And-Variables/Exercise/13-LeftRight/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MORE_02.LEftRight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                string first = Console.ReadLine();
+                string second = Console.ReadLine();
+
+                int one = int.Parse(first);
+                int two = int.Parse(second);
+
+                int sum = 0;
+                if (one > two)
+                {
+                    for (int j = 0; j < first.Length; j++)
+                    {
+                        int currentNumber = (int)Char.GetNumericValue(first[j]);
+                        sum += currentNumber;
+                    }
+                }
+                else if (one < two)
+                {
+                    for (int j = 0; j < second.Length; j++)
+                    {
+                        int currentNumber = (int)Char.GetNumericValue(second[j]);
+                        sum += currentNumber;
+                    }
+                }
+
+                Console.WriteLine(sum);            
+            }
+        }
+    }
+}
