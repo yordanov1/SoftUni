e607a11ef12ee4d3e41e21a0d7905c1ece69041f
yordanov1 <yordanov8801@gmail.com>
2021-01-10 17:21:10 +0200

    Add Data Types

---
+++ C#-Fundamentals-Module/Data-Types-And-Variables/Lab/05-ReverceChar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LAB_06.ReverceChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+
+            Console.Write(c + " ");
+            Console.Write(b + " ");
+            Console.Write(a);
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/Data-Types-And-Variables/Lab/06-ConcatNames/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LAB_06.ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOne = Console.ReadLine();
+            string nameTwo = Console.ReadLine();
+            string symbol = Console.ReadLine();
+
+            Console.WriteLine($"{nameOne}{symbol}{nameTwo}");
+        }
+    }
+}
