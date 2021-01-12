aa2c0abb7fa241bdc87d480800fc7b4059eeea1f
yordanov1 <yordanov8801@gmail.com>
2021-01-12 22:08:04 +0200

    Add Arrays

---
+++ C#-Fundamentals-Module/Arrays/Exercise/01-Vagon/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EXER_01.Wagon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] wagon = new int[n];
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                wagon[i] = int.Parse(Console.ReadLine());
+
+                sum += wagon[i];
+
+            }
+
+            Console.WriteLine(string.Join(" ", wagon));
+
+            Console.WriteLine(sum);
+        }
+    }
+}
