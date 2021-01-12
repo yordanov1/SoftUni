9652db45decb515b9f4402c011fd854e905fac08
yordanov1 <yordanov8801@gmail.com>
2021-01-12 22:13:28 +0200

    Add Arrays

---
+++ C#-Fundamentals-Module/03-Arrays/Exercise/02-ComonElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace EXER_02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] arrOne = Console.ReadLine().Split();
+            string[] arrTwo = Console.ReadLine().Split();
+
+            for (int i = 0; i < arrTwo.Length; i++)
+            {
+                for (int j = 0; j < arrOne.Length; j++)
+                {
+                    if (arrTwo[i] == arrOne[j])
+                    {
+                        Console.Write(arrTwo[i] + " ");
+                    }
+
+                }
+            }
+          
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/03-Arrays/Exercise/03-ZigZag/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace EXER_03.ZigZag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr1 = new int[n];
+            int[] arr2 = new int[n];
+
+            int a1 = 0;
+            int a2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if(i % 2 == 0)
+                {
+                    a1 = 0;
+                    a2 = 1;
+                }
+                else
+                {
+                    a1 = 1;
+                    a2 = 0;
+                }
+                
+                arr1[i] = input[a1];
+                arr2[i] = input[a2];
+            }
+
+            Console.WriteLine(string.Join(" ", arr1));
+            Console.WriteLine(string.Join(" ", arr2));
+        }
+    }
+}
