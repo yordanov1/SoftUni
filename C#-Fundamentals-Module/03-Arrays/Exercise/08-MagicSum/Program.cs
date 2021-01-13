c89f33d656d72961f367908a4b23310bccc02bcd
yordanov1 <yordanov8801@gmail.com>
2021-01-13 12:31:13 +0200

    Add Arrays

---
+++ C#-Fundamentals-Module/03-Arrays/Exercise/08-MagicSum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace EXER_08.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int current = arr[i];
+
+                for (int j = i + 1 ; j < arr.Length; j++)
+                {
+                    int switcher = arr[j];
+
+
+                    if(current + switcher == number)
+                    {
+
+                        Console.WriteLine($"{current} {switcher}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/03-Arrays/Exercise/09-KaminoFactory/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace EXER_09.KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+            int bestCount = 0;
+            int counter = 0;
+            int bestBegunIndex = 0;
+            int bestSum = 0;
+            string bestSequence = "";
+            int bestCounter = 0;
+
+            while((input = Console.ReadLine()) != "Clone them!")
+            {
+                string sequence = input.Replace("!", "");
+
+                string[] dnaParts = sequence.Split("0", StringSplitOptions.RemoveEmptyEntries);
+
+                int count = 0;
+                int sum = 0;
+                string bestSubSequence = "";
+                counter++;
+
+                foreach (string dnaPart in dnaParts)
+                {
+                    if(dnaPart.Length > count)
+                    {
+                        count = dnaPart.Length;
+                        bestSubSequence = dnaPart;
+                    }
+                    sum += dnaPart.Length;
+                }
+
+                int beginIndex = sequence.IndexOf(bestSubSequence);
+
+                if(count > bestCount ||
+                    (count == bestCount && beginIndex < bestBegunIndex)||
+                    (count == bestCount && beginIndex == bestBegunIndex && sum > bestSum)||
+                    counter == 1)
+                {
+                    bestCount = count;
+                    bestSequence = sequence;
+                    bestBegunIndex = beginIndex;
+                    bestSum = sum;
+                    bestCounter = counter;
+                }
+            }
+
+            char[] result = bestSequence.ToCharArray();
+
+            Console.WriteLine($"Best DNA sample {bestCounter} with sum: {bestSum}.");
+            Console.WriteLine($"{string.Join(" ", result)} ");
+        }
+
+    }
+}
