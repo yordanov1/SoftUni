ec5058597d0e210ee9d63458f150e5fa2d0e5c0c
yordanov1 <yordanov8801@gmail.com>
2021-01-25 10:58:25 +0200

    Add Lists

---
+++ C#-Fundamentals-Module/05-Lists/Lab/04-ListOfProducts/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB_04.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> listAZ = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                listAZ.Add(Console.ReadLine());
+            }
+
+            listAZ.Sort();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}.{listAZ[i]}");
+            }
+
+        }
+    }
+}
