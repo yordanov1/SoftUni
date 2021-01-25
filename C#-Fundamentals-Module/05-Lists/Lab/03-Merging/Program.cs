f59be7c210208c6bb06d0b32a4c2e4e8ddccda43
yordanov1 <yordanov8801@gmail.com>
2021-01-25 10:56:06 +0200

    Add Lists

---
+++ C#-Fundamentals-Module/05-Lists/Lab/03-Merging/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LAB_03.Merging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> second = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> sum = new List<int>();
+
+            int bigger = Math.Max(first.Count, second.Count);
+
+            for (int i = 0; i < bigger ; i++)
+            {
+                if (first.Count > i)
+                {
+                    sum.Add(first[i]);
+                }
+                if (second.Count > i)
+                {
+                    sum.Add(second[i]);
+
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",sum));
+        }
+    }
+}
