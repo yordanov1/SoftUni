aa93bcd844c9fa060e36d708f414e9f501a6eb1b
yordanov1 <yordanov8801@gmail.com>
2021-01-27 23:13:41 +0200

    Add Lists

---
+++ C#-Fundamentals-Module/05-Lists/Lab/05-Remove/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LAB_05.Remove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                                     .Split()
+                                     .Select(int.Parse)
+                                     .ToList();
+
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                if (input[i] < 0)
+                {
+                    input.RemoveAt(i--);
+                }
+            }
+
+            if (input.Count == 0)
+            {
+                Console.WriteLine("empty"); 
+            }
+            else
+            {
+                input.Reverse();
+
+                Console.WriteLine(string.Join(" ", input));
+            }
+        }
+    }
+}
