2e1e944b29af677c753e5b76743ed0410e7cb877
yordanov1 <yordanov8801@gmail.com>
2021-01-30 14:03:30 +0200

    Add Lists

---
+++ C#-Fundamentals-Module/05-Lists/Exercise/01-Train/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int maxPassengers = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                List<string> comandSEP = command.Split().ToList();
+
+                    if (comandSEP[0] == "Add")
+                    {
+                        int newVagon = int.Parse(comandSEP[1]);
+                        wagons.Add(newVagon);
+                    }
+                    else
+                    {
+                        int passengers = int.Parse(comandSEP[0]);
+
+                       for (int i = 0; i < wagons.Count; i++)
+                       {                       
+                          if ((wagons[i] + passengers) <= maxPassengers)
+                          {
+                            wagons[i] += passengers;
+                            break;
+                          }
+                       }
+                    }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
