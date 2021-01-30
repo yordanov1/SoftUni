4f647336d62604752d5f90156adae75cf123fb9b
yordanov1 <yordanov8801@gmail.com>
2021-01-30 14:05:16 +0200

    Add Lists

---
+++ C#-Fundamentals-Module/05-Lists/Exercise/02-ChangeTheList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_02.ChangeTheList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> element = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string[] command = Console.ReadLine().Split();
+
+            while (command[0] != "end")
+            {
+                if (command[0] == "Delete")
+                {
+                    int elementToDel = int.Parse(command[1]);
+                    
+                    element.RemoveAll(i => i == elementToDel);
+                }
+                else if(command[0] == "Insert")
+                {
+                    int position = int.Parse(command[2]);
+                    int numberToPut = int.Parse(command[1]);
+
+                    element.Insert(position, numberToPut);
+                }
+
+                command = Console.ReadLine().Split();
+            }
+
+            Console.WriteLine(string.Join(" ",element));
+        }
+    }
+}
