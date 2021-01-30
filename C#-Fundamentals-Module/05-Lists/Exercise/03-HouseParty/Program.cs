88ed1860757f0a281b63f815d5dd6ebf0041a6f3
yordanov1 <yordanov8801@gmail.com>
2021-01-30 14:06:14 +0200

    Add Lists

---
+++ C#-Fundamentals-Module/05-Lists/Exercise/03-HouseParty/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_03.HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> persons = new List<string>();
+            
+            for (int i = 1; i <= n; i++)
+            {
+                List<string> command = Console.ReadLine().Split().ToList();
+
+                string nameTo = command[0];
+
+                if (command.Count == 3)
+                {
+                    if (persons.Contains(nameTo))
+                    {
+                        Console.WriteLine($"{nameTo} is already in the list!");
+                    }
+                    else
+                    {
+                        persons.Add(nameTo);
+                    }
+                }
+                else if (command.Count == 4)
+                {
+                    if (persons.Contains(nameTo))
+                    {
+                        persons.Remove(nameTo);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{nameTo} is not in the list!");
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, persons));
+        }
+    }
+}
