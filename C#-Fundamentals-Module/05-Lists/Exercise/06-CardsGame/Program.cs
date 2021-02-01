717987c214ec020706ac70a8cb9b332783cfbd11
yordanov1 <yordanov8801@gmail.com>
2021-02-01 13:16:15 +0200

    Add Lists

---
+++ C#-Fundamentals-Module/05-Lists/Exercise/06-CardsGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_06.CardsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> playerONE = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> playerTWO = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (playerONE.Count != 0 && playerTWO.Count != 0 )
+            {
+                if (playerONE[0] > playerTWO[0])
+                {
+                    playerONE.Add(playerONE[0]);
+                    playerONE.Add(playerTWO[0]);
+                    playerONE.RemoveAt(0);
+                    playerTWO.RemoveAt(0);
+                }
+                else if(playerONE[0] < playerTWO[0])
+                {
+                    playerTWO.Add(playerTWO[0]);
+                    playerTWO.Add(playerONE[0]);
+                    playerONE.RemoveAt(0);
+                    playerTWO.RemoveAt(0);
+                }
+                else if (playerONE[0] == playerTWO[0])
+                {
+                    playerONE.RemoveAt(0);
+                    playerTWO.RemoveAt(0);
+                }
+            }
+
+            if (playerONE.Count != 0)
+            {
+                int sum = playerONE.Sum();
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            }
+            else
+            {
+                int sum = playerTWO.Sum();
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            }          
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/05-Lists/Exercise/07-AppendArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_07.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            List<string> list = Console.ReadLine().Split("|").ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                string word = list[i];
+                string newWord = string.Empty;
+
+                for (int j = 0; j < word.Length; j++)
+                {                    
+                    if (word[j] != ' ')
+                    {
+                        newWord +=  word[j];
+                        newWord += ' ';
+                    }
+                }
+                
+                list.Insert(i, newWord);
+                list.RemoveAt((i + 1));
+            }
+
+            list.Reverse();
+            Console.WriteLine(string.Join("", list));
+        }
+    }
+}
