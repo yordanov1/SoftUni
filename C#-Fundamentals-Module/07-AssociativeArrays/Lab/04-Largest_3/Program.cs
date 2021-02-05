8396f17dbb4fac5299b5a45c38bc82fd83bcadb2
yordanov1 <yordanov8801@gmail.com>
2021-02-05 11:40:41 +0200

    Add Dictionaries

---
+++ C#-Fundamentals-Module/07-AssociativeArrays/Lab/04-Largest_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4_LAB_Largest_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                       .Split()
+                                       .Select(int.Parse)
+                                       .OrderByDescending(i => i)
+                                       .Take(3)
+                                       .ToList();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/07-AssociativeArrays/Lab/05-WordFilter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5_WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                                        .Split()
+                                        .Where(i => i.Length % 2 == 0)
+                                        .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine,words));
+        }
+    }
+}
