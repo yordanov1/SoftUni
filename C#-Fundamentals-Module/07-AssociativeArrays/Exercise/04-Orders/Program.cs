217942c92368cd5d47246397947ce7819dc5cad6
yordanov1 <yordanov8801@gmail.com>
2021-02-09 10:52:45 +0200

    Add Dictionaries

---
+++ C#-Fundamentals-Module/07-AssociativeArrays/Exercise/04-Orders/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_4_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> products = Console.ReadLine().Split().ToList();
+
+            Dictionary<string, List<double>> book = new Dictionary<string, List<double>>();
+
+            while (products[0] != "buy")
+            {
+                string product = products[0];
+                double price = double.Parse(products[1]);
+                double quantity = double.Parse(products[2]);
+
+                if (!book.ContainsKey(product))
+                {
+                    book.Add(product, new List<double> { price , quantity});
+                }
+                else
+                {
+                    book[product][0] = price;
+                    book[product][1] += quantity;                   
+                }
+
+                products = Console.ReadLine().Split().ToList();
+            }
+
+            foreach (var bookche in book)
+            {               
+                double multiplyed = bookche.Value[0] * bookche.Value[1];
+
+                Console.WriteLine($"{ bookche.Key} -> {multiplyed:f2}");
+            }
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/07-AssociativeArrays/Exercise/05-SoftUniParking/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_5_SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> inParking = new Dictionary<string, string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                List<string> commands = Console.ReadLine().Split().ToList();
+
+                if (commands.Count < 3)
+                {
+                    commands.Add("42");
+                }
+                    string command = commands[0];
+                    string person = commands[1];
+                    string number = commands[2];
+
+                if (command == "register")
+                {
+                    if (inParking.ContainsKey(person))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {number}");
+                    }
+                    else
+                    {
+                        inParking.Add(person, number);
+                        Console.WriteLine($"{person} registered {number} successfully");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    if (inParking.ContainsKey(person))
+                    {
+                        inParking.Remove(person);
+                        Console.WriteLine($"{person} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {person} not found");
+                    }
+                }
+            }
+
+            foreach (var person in inParking)
+            {
+                Console.WriteLine($"{person.Key} => {person.Value}");
+            }
+
+        }
+    }
+}
