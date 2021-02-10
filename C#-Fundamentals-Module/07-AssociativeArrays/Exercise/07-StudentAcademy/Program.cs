001576ecfef4741fb600309caad44588cd334f4d
yordanov1 <yordanov8801@gmail.com>
2021-02-10 12:28:21 +0200

    Add Dictionary

---
+++ C#-Fundamentals-Module/07-AssociativeArrays/Exercise/07-StudentAcademy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EXER_7_StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentsDic = new Dictionary<string, List<double>>();
+            Dictionary<string, double> average = new Dictionary<string, double>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!studentsDic.ContainsKey(student))
+                {
+                    studentsDic.Add(student, new List<double>());
+                }
+                studentsDic[student].Add(grade);
+            }
+
+            foreach (var item in studentsDic)
+            {
+                double hoho = item.Value.Average();
+                if (hoho >= 4.5)
+                {
+                    average.Add(item.Key, hoho);
+                }               
+            }
+
+            foreach (var item in average.OrderByDescending(x => x.Value))
+            {                
+                Console.WriteLine($"{item.Key} -> {item.Value:f2}");
+            }
+
+        }
+    }
+}
---
+++ C#-Fundamentals-Module/07-AssociativeArrays/Exercise/08-CompanyUsers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace EXER_8_CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> companyAndId = Console.ReadLine().Split(" -> ").ToList();
+
+            SortedDictionary<string, List<string>> dictionary = new SortedDictionary<string, List<string>>();
+
+            while (companyAndId[0] != "End")
+            {
+                string company = companyAndId[0];
+                string id = companyAndId[1];
+
+                if (!dictionary.ContainsKey(company))
+                {
+                    dictionary.Add(company, new List<string>());
+                    dictionary[company].Add(id);
+                }
+                else 
+                {
+                    if (!dictionary[company].Contains(id))
+                    {
+                        dictionary[company].Add(id);
+                    }
+                }
+
+                companyAndId = Console.ReadLine().Split(" -> ").ToList();
+            }
+
+            foreach (var comp in dictionary)
+            {
+                Console.WriteLine($"{comp.Key}");
+
+                foreach (var aid in comp.Value)
+                {
+                    Console.WriteLine($"-- {aid}");
+                }
+            }
+
+        }
+    }
+}
