64a922a689eae1635d51328e30878d22ce299ccc
yordanov1 <yordanov8801@gmail.com>
2021-02-11 10:52:53 +0200

    Add Text Processing

---
+++ C#-Fundamentals-Module/08-TextProcessing/Lab/05-DigitsLettersOther/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace LAB_5_DigitsLettersOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder digit = new StringBuilder();
+            StringBuilder letter = new StringBuilder();
+            StringBuilder symbol = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    digit.Append(text[i]);
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    letter.Append(text[i]);
+                }
+                else
+                {
+                    symbol.Append(text[i]);
+                }
+            }
+
+            Console.WriteLine(digit);
+            Console.WriteLine(letter);
+            Console.WriteLine(symbol);
+        }
+    }
+}
