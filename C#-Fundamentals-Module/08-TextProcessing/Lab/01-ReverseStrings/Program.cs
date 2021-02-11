85691c741240bf0f69bea6209b8d86880adefc8e
yordanov1 <yordanov8801@gmail.com>
2021-02-11 10:48:14 +0200

    Add Text Processing

---
+++ C#-Fundamentals-Module/08-TextProcessing/Lab/01-ReverseStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace LAB_1_ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();            
+
+            while (input != "end")
+            {
+                List<char> text = new List<char>();
+                List<char> reverse = new List<char>();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    text.Add(input[i]);
+                }
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    reverse.Add(input[i]);
+                }
+
+                reverse.Reverse();
+
+                Console.WriteLine($"{string.Join("", text)} = {string.Join("", reverse)}");
+
+                input = Console.ReadLine();
+            }          
+        }
+    }
+}
