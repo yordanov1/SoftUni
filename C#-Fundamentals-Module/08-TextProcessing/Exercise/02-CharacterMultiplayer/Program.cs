1195f32819bf201e3806d1d00953ddbc3e98b12a
yordanov1 <yordanov8801@gmail.com>
2021-02-13 10:38:24 +0200

    Add Text Processing

---
+++ C#-Fundamentals-Module/08-TextProcessing/Exercise/02-CharacterMultiplayer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EXER_2_CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string big = string.Empty;
+            string small = string.Empty;
+
+            int result = 0;
+
+            if (input[0].Length >= input[1].Length)
+            {
+                big = input[0];
+                small = input[1];
+            }
+            else
+            {
+                big = input[1];
+                small = input[0];
+            }
+
+            for (int i = 0; i < small.Length; i++)
+            {
+                result += big[i] * small[i];
+            }
+
+            if (big != small)
+            {
+                for (int j = small.Length; j < big.Length; j++)
+                {
+                    result += big[j];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
