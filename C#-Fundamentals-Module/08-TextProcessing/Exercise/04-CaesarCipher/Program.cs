f7323c196dbeccdb889f504fa397a1f6fcaf2a30
yordanov1 <yordanov8801@gmail.com>
2021-02-13 10:39:59 +0200

    Add Text Processing

---
+++ C#-Fundamentals-Module/08-TextProcessing/Exercise/04-CaesarCipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EXER_4._CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder shifterText = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                shifterText.Append((char)(text[i] + 3));
+            }
+
+            Console.WriteLine(shifterText);
+        }
+    }
+}
