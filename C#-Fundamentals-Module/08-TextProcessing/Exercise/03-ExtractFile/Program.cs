334c64ed492da2dfe2db11b59acf04ab1a6065b3
yordanov1 <yordanov8801@gmail.com>
2021-02-13 10:39:13 +0200

    Add Text Processing

---
+++ C#-Fundamentals-Module/08-TextProcessing/Exercise/03-ExtractFile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EXER_3_ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> directory = Console.ReadLine().Split("\\").ToList();
+            List<string> lastTwo = directory[directory.Count - 1].Split(".").ToList();
+
+            Console.WriteLine($"File name: {lastTwo[0]}");
+            Console.WriteLine($"File extension: {lastTwo[1]}");            
+        }
+    }
+}
