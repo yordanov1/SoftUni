02ac0b766989bd991d194cb83534a1088cb51743
yordanov1 <yordanov8801@gmail.com>
2021-02-14 10:17:00 +0200

    Add Regex

---
+++ C#-Fundamentals-Module/09-RegularExpressions/Lab/01-MatchFullName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LAB_1_MatchFullName
+{
+    class Program
+    {
+        public static object MathColection { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string patern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            Regex regex = new Regex(patern);
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match} ");
+            }
+        }
+    }
+}
