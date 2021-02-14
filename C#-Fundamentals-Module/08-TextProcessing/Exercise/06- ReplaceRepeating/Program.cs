58de5ac4332b38fbfe73ba540102c7042c443de2
yordanov1 <yordanov8801@gmail.com>
2021-02-14 10:09:59 +0200

    Add Text Processing

---
+++ C#-Fundamentals-Module/08-TextProcessing/Exercise/06- ReplaceRepeating/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EXER_6__ReplaceRepeating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();            
+
+            Console.Write(text[0]);
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (text[i] != text[i - 1])
+                {
+                    Console.Write(text[i]);
+                }
+            }
+        }
+    }
+}
