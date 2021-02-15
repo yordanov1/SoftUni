e1ba7ba247ef81eb71e1c0ed2bd243d2c4c8b57a
yordanov1 <yordanov8801@gmail.com>
2021-02-15 10:33:57 +0200

    Add Regex

---
+++ C#-Fundamentals-Module/09-RegularExpressions/Lab/02-MatchPhone/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace LAB_2_MatchPhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\+359(-? ?)2(.)[0-9]{3}(\1)[0-9]{4}\b";
+            
+            Regex regex = new Regex(patern);
+            string input = Console.ReadLine();
+
+            MatchCollection willMach = regex.Matches(input);
+            
+            Console.WriteLine(string.Join(", ", willMach));
+        }
+    }
+}
