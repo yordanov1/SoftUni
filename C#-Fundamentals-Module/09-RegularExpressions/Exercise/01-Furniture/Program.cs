e7104e61661542be7b87791e8aa3dae78b790b0e
yordanov1 <yordanov8801@gmail.com>
2021-02-18 12:56:58 +0200

    Add Regex

---
+++ C#-Fundamentals-Module/09-RegularExpressions/Exercise/01-Furniture/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace EXER_1_Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(?<furniture>[A-Za-z]+)<<(?<price>[0-9]+\.?[0-9]+)!(?<count>[0-9]{1,})";
+            Regex regexxx = new Regex(pattern);
+            double price = 0;
+
+            string input = Console.ReadLine();
+
+            List<string> AllInList = new List<string>();
+           
+            while (input != "Purchase")
+            {                
+                Match allIn = regexxx.Match(input);
+
+                if (allIn.Success)
+                {
+                    AllInList.Add(allIn.Groups[1].Value);
+
+                    price += double.Parse(allIn.Groups["price"].Value) * double.Parse(allIn.Groups["count"].Value);                    
+                }
+
+                input = Console.ReadLine();                
+            }
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (var item in AllInList)
+            {              
+                Console.WriteLine(item);
+            }
+
+            if (AllInList.Count > 0)
+            {
+                Console.WriteLine($"Total money spend: {price:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Total money spend: 0.00");
+            }
+        }
+    }
+}
