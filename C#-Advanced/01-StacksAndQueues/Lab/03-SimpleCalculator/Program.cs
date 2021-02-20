066d00abcf1ebd4fb8065ceb0b059952a0bf1975
yordanov1 <yordanov8801@gmail.com>
2021-02-20 15:36:24 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Lab/03-SimpleCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputNumbers = Console.ReadLine()
+                                           .Split()
+                                           .Reverse()
+                                           .ToArray();
+
+            Stack<string> stack = new Stack<string>(inputNumbers);
+
+            while (stack.Count > 1)
+            {
+                int firstNumber = int.Parse(stack.Pop());
+                string symbol = stack.Pop();
+                int secondNumber = int.Parse(stack.Pop());
+
+
+                if (symbol == "+")
+                {
+                    stack.Push((firstNumber + secondNumber).ToString());
+                }
+                else if (symbol == "-")
+                {
+                    stack.Push((firstNumber - secondNumber).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
