90ed65ce29710da2939341f24d501e0833ccd941
yordanov1 <yordanov8801@gmail.com>
2021-02-20 15:31:44 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Lab/01-ReverseStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                stack.Push(inputString[i]);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
