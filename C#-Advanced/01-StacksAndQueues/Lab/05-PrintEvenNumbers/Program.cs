68294dcfc46efe62c6900422b10b2f63a181df92
yordanov1 <yordanov8801@gmail.com>
2021-02-21 09:54:46 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Lab/05-PrintEvenNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> evenNumbers = new Queue<int>();           
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                int currentNumber = numbers[i];
+
+                if (currentNumber % 2 == 0)
+                {
+                    evenNumbers.Enqueue(currentNumber);
+                }    
+            }
+            
+            Console.WriteLine(string.Join(", ",evenNumbers));           
+        }
+    }
+}
