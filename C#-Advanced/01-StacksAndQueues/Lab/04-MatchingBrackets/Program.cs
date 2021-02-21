6bc8f38f3793cb33b0b52afef0f510028d59c6d2
yordanov1 <yordanov8801@gmail.com>
2021-02-21 09:53:57 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Lab/04-MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lab_04.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputNumbers = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < inputNumbers.Length; i++)
+            {
+                if (inputNumbers[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                if (inputNumbers[i] == ')')
+                {
+                    int start = stack.Pop();
+                    Console.WriteLine(inputNumbers.Substring(start, i - start + 1));
+                }
+            }
+
+        }
+    }
+}
