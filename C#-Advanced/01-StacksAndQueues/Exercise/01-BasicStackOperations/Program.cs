2adade36a1e4f00c8015ee63c8a1d3dfbabfbf2b
yordanov1 <yordanov8801@gmail.com>
2021-02-23 12:00:48 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Exercise/01-BasicStackOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exer_01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] NSX = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> numbers = new Stack<int>(inputNumbers);
+
+            if (numbers.Count - NSX[1] >= 0)
+            {
+                for (int i = 0; i < NSX[1]; i++)
+                {
+                    numbers.Pop();
+                }
+            }
+            else
+            {
+                numbers.Clear();
+            }
+
+            if (numbers.Count != 0)
+            {
+                if (numbers.Contains(NSX[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }            
+        }
+    }
+}
