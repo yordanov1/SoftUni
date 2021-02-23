e1ccca442b925417634c84e5b775b1395d3c2351
yordanov1 <yordanov8801@gmail.com>
2021-02-23 11:57:28 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Lab/07-HotPotato/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split().ToArray();
+            int potatoMoves = int.Parse(Console.ReadLine());
+
+            Queue<string> childernRotation = new Queue<string>(children);
+
+            while (childernRotation.Count > 1)
+            {
+                for (int i = 1; i < potatoMoves; i++)
+                {
+                    childernRotation.Enqueue(childernRotation.Dequeue());
+                }
+                Console.WriteLine($"Removed {childernRotation.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {childernRotation.Dequeue()}");
+        }
+    }
+}
