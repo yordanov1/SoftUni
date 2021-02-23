16537d6c64a3aebdedc1fe83bfa08a51bb494037
yordanov1 <yordanov8801@gmail.com>
2021-02-23 11:56:48 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Lab/06-Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string incomingPeople = Console.ReadLine();
+
+            Queue<string> peopleOnQueue = new Queue<string>();
+
+            while (incomingPeople != "End")
+            {
+                if (incomingPeople == "Paid")
+                {
+                    foreach (var item in peopleOnQueue)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    peopleOnQueue.Clear();
+                }
+                else
+                {
+                    peopleOnQueue.Enqueue(incomingPeople);
+                }
+
+                incomingPeople = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{peopleOnQueue.Count} people remaining.");
+        }
+    }
+}
