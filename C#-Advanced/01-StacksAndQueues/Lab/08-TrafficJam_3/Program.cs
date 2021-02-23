7af69a48b0b23cf00abf2a4c9f8817d5825b1c69
yordanov1 <yordanov8801@gmail.com>
2021-02-23 11:58:18 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Lab/08-TrafficJam_3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Lab_08.TrafficJam_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int passingCars = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            Queue<string> queue = new Queue<string>();
+
+            int counter = 0;
+
+            while (input != "end")
+            {
+                int count = queue.Count;
+
+                if (input == "green")
+                {
+                    if (queue.Count >= passingCars)
+                    {
+                        for (int i = 0; i < passingCars; i++)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            counter++;                           
+                        }             
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
