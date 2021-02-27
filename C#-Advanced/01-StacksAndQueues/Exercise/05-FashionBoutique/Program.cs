422e706ab4da296c8a26c81de2d9ea0d3ac5ae25
yordanov1 <yordanov8801@gmail.com>
2021-02-27 10:56:11 +0200

    Add

---
+++ C#-Advanced/01-StacksAndQueues/Exercise/05-FashionBoutique/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exer_05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputClothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            int clothesLeft = capacity;
+
+            int counter = 0;         
+
+            Stack<int> clothes = new Stack<int>(inputClothes);
+
+            while (clothes.Count != 0)
+            {
+                if (clothesLeft - clothes.Peek() < 0)
+                {
+                    clothesLeft = capacity;
+                    counter++;
+                }
+                else
+                {                   
+                    clothesLeft -= clothes.Pop();
+                }
+            }
+
+            counter++;
+
+            Console.WriteLine(counter);
+        }
+    }
+}
