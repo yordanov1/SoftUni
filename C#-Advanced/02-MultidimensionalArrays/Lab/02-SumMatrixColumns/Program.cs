ec99ef2f86f999d191bcc4895e1ce44c5628c502
yordanov1 <yordanov8801@gmail.com>
2021-03-06 11:03:11 +0200

    Add

---
+++ C#-Advanced/02-MultidimensionalArrays/Lab/02-SumMatrixColumns/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_2.SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+       
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = sizes[0];
+            int cols = sizes[1];
+
+            int[,] matrix = FillMatrix(rows, cols);
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);                
+            }
+        }
+
+        static int[,] FillMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var rowData = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            return matrix;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        static int SumMatrix(int[,] matrix)
+        {
+            int sum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    sum += matrix[row, col];
+                }
+            }
+            return sum;
+        }
+    }
+}
