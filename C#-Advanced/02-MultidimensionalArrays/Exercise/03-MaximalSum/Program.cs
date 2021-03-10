dce3008510ad3a9dcd9368d615c8643454236f2d
yordanov1 <yordanov8801@gmail.com>
2021-03-10 11:20:15 +0200

    Add

---
+++ C#-Advanced/02-MultidimensionalArrays/Exercise/03-MaximalSum/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+namespace Exer_03.MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              int[] sizes = Console.ReadLine()
+                                   .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+           
+              int[,] matrix = FillMatrix(sizes[0], sizes[1]);
+              int[,] biggestMatrix3x3 = new int[3, 3];
+              int biggestSum = int.MinValue;
+
+              for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+              {
+                  for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                  {
+                      int sum = matrix[row, col] 
+                              + matrix[row, col + 1] 
+                              + matrix[row, col + 2] 
+                              + matrix[row + 1, col] 
+                              + matrix[row + 1, col + 1] 
+                              + matrix[row + 1, col + 2] 
+                              + matrix[row + 2, col] 
+                              + matrix[row + 2, col + 1] 
+                              + matrix[row + 2, col + 2];
+
+                      if (biggestSum < sum)
+                      {
+                          biggestSum = sum;
+
+                          for (int i = 0; i < 3; i++)
+                          {
+                              for (int j = 0; j < 3; j++)
+                              {
+                                  biggestMatrix3x3[i, j] = matrix[row + i, col + j];
+                              }
+                          }
+                      }
+                  }
+              }
+
+              Console.WriteLine($"Sum = {biggestSum}");
+              PrintMatrix(biggestMatrix3x3);           
+        }
+
+        static int[,] FillMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var rowData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            return matrix;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#-Advanced/02-MultidimensionalArrays/Exercise/04-MatrixShuffling/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace Exer_04.MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            string[,] matrix = FillMatrix(rows, cols);
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+                if (!validateCommand(command, rows, cols))
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                string[] commands = command.Split();
+                int rowIndexFirst = int.Parse(commands[1]);
+                int colIndexFirst = int.Parse(commands[2]);
+                int rowIndexSecond = int.Parse(commands[3]);
+                int colIndexSecond = int.Parse(commands[4]);
+
+                string firstElement = matrix[rowIndexFirst, colIndexFirst];
+                string secondElement = matrix[rowIndexSecond, colIndexSecond];
+
+                matrix[rowIndexSecond, colIndexSecond] = firstElement;
+                matrix[rowIndexFirst, colIndexFirst] = secondElement;
+
+                PrintMatrix(matrix);
+            }
+        }
+
+        private static bool validateCommand(string command, int rows , int cols)
+        {           
+            string[] commands = command.Split();
+            if (commands.Length == 5 && commands[0] == "swap"
+                && int.Parse(commands[1]) <= rows 
+                && int.Parse(commands[2]) <= cols
+                && int.Parse(commands[3]) <= rows
+                && int.Parse(commands[4]) <= cols)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static string[,] FillMatrix(int rows, int cols)
+        {
+            string[,] matrix = new string[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var rowData = Console.ReadLine().Split().ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+            return matrix;
+        }
+
+        static void PrintMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
