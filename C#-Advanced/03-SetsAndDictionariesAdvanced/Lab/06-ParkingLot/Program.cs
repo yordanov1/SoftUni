e3687947540db207a7ad201ef1da17d289807bb7
yordanov1 <yordanov8801@gmail.com>
2021-03-12 11:36:07 +0200

    Add

---
+++ C#-Advanced/03-SetsAndDictionariesAdvanced/Lab/06-ParkingLot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> allCarNumbers = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] numbers = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string direction = numbers[0];
+                string number = numbers[1];
+
+                if (direction == "IN")
+                {
+                    allCarNumbers.Add(number);
+                }
+                else if (direction == "OUT")
+                {
+                    allCarNumbers.Remove(number);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (allCarNumbers.Count > 0)
+            {
+                foreach (var number in allCarNumbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else if (allCarNumbers.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/03-SetsAndDictionariesAdvanced/Lab/07-SoftUniParty/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "PARTY")
+            {
+                if (input[0] == '1' || input[0] == '2' || input[0] == '3' 
+                                    || input[0] == '4' || input[0] == '5' 
+                                    || input[0] == '6' || input[0] == '6'
+                                    || input[0] == '7' || input[0] == '8'
+                                    || input[0] == '9' || input[0] == '0')
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regularGuests.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            string secondInput = Console.ReadLine();
+
+            while (secondInput != "END")
+            {
+                if (secondInput[0] == '1' || secondInput[0] == '2' || secondInput[0] == '3'
+                                          || secondInput[0] == '4' || secondInput[0] == '5'
+                                          || secondInput[0] == '6' || secondInput[0] == '6'
+                                          || secondInput[0] == '7' || secondInput[0] == '8'
+                                          || secondInput[0] == '9' || secondInput[0] == '0')
+                {
+                    vipGuests.Remove(secondInput);
+                }
+                else
+                {
+                    regularGuests.Remove(secondInput);
+                }
+                secondInput = Console.ReadLine();
+            }
+
+            int guestsCount = vipGuests.Count + regularGuests.Count;
+
+            Console.WriteLine(guestsCount);
+
+            foreach (var vip in vipGuests)
+            {
+                Console.WriteLine(vip);
+            }
+            foreach (var regular in regularGuests)
+            {
+                Console.WriteLine(regular);
+            }
+
+        }
+    }
+}
