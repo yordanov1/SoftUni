52b2326b57eceea13af8b7b7c135ce58c44aef2d
yordanov1 <yordanov8801@gmail.com>
2021-03-17 11:33:42 +0200

    Add

---
+++ C#-Advanced/03-SetsAndDictionariesAdvanced/Exercise/03-PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exer_03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> uniqueElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] newElements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                for (int j = 0; j < newElements.Length; j++)
+                {
+                    uniqueElements.Add(newElements[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", uniqueElements));
+        }
+    }
+}
---
+++ C#-Advanced/03-SetsAndDictionariesAdvanced/Exercise/04-EvenTimes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int newNumber = int.Parse(Console.ReadLine());
+
+                if (!dictionary.ContainsKey(newNumber))
+                {
+                    dictionary.Add(newNumber, 0);
+                }
+
+                dictionary[newNumber]++;
+            }
+            
+
+            foreach (var number in dictionary)
+            {
+                if (number.Value % 2 == 0)
+                {
+                    Console.WriteLine(number.Key);
+                }
+            }
+            
+        }
+    }
+}
