5089d14ff39fc86e0fb13270e270a10f26d375d9
yordanov1 <yordanov8801@gmail.com>
2021-03-17 11:34:48 +0200

    Add

---
+++ C#-Advanced/03-SetsAndDictionariesAdvanced/Exercise/05-CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exer_05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            SortedDictionary<char, int> colection = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!colection.ContainsKey(input[i]))
+                {
+                    colection.Add(input[i], 0);
+                }
+
+                colection[input[i]]++;
+            }
+
+            foreach (var symbol in colection)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/03-SetsAndDictionariesAdvanced/Exercise/06-Wardrobe/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exer_06.Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> collection = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string colour = input[0];
+                string[] garment = input[1].Split(",",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+
+                if (!collection.ContainsKey(colour))
+                {
+                    collection.Add(colour, new Dictionary<string, int>());
+                }
+
+                for (int j = 0; j < garment.Length; j++)
+                {
+                    if (!collection[colour].ContainsKey(garment[j]))
+                    {
+                        collection[colour].Add(garment[j], 0);
+                    }
+
+                    collection[colour][garment[j]]++;
+                }
+            }
+
+            string[] searchGarment = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            foreach (var colour in collection)
+            {
+                Console.WriteLine($"{colour.Key} clothes:");
+
+                foreach (var garment in colour.Value)
+                {
+                    if (searchGarment[0] == colour.Key && searchGarment[1] == garment.Key)
+                    {
+                        Console.WriteLine($"* {garment.Key} - {garment.Value} (found!)"); 
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {garment.Key} - {garment.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
