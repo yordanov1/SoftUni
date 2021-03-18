40f2cf8c639e145b16547d6bc82d92c550b1a6f0
yordanov1 <yordanov8801@gmail.com>
2021-03-18 22:07:31 +0200

    Add

---
+++ C#-Advanced/04-FunctionalProgramming/Lab/04-AddVAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab_04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] input = Console.ReadLine()
+                                     .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(decimal.Parse)
+                                     .Select(x => x * 1.2m)
+                                     .ToArray();
+
+            foreach (var number in input)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+    }
+}
