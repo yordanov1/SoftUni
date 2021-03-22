01aa6b9ed42f0e6de2123c382d36eb8da1d16cfa
yordanov1 <yordanov8801@gmail.com>
2021-03-22 20:12:44 +0200

    Add

---
+++ C#-Advanced/04-FunctionalProgramming/Exercise/05-AppliedArithmetics/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Exer_05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = numbers
+                        .Select(x => x + 1)
+                        .ToList();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers
+                        .Select(x => x * 2)
+                        .ToList();
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers
+                        .Select(x => x - 1)
+                        .ToList();
+                }
+                else if (command == "print")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C#-Advanced/04-FunctionalProgramming/Exercise/06-ReverseAndExclude/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exer_06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            int divisor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(" ", numbers.Where(x => x % divisor != 0)));
+        }
+    }
+}
