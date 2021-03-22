1d01efd94c23d1e339546c73c3b8f01aaf4e828d
yordanov1 <yordanov8801@gmail.com>
2021-03-22 20:09:38 +0200

    Add

---
+++ C#-Advanced/04-FunctionalProgramming/Exercise/01-ActionPoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab_01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Action<string[]> printName = a => Console.WriteLine(string.Join(Environment.NewLine, a));
+
+            printName(names);
+        }
+    }
+}
