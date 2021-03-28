ba790f1f2245262c8043356bf8f7b84c41dde0c6
yordanov1 <yordanov8801@gmail.com>
2021-03-28 13:57:27 +0300

    Add

---
+++ C#-Advanced/07-IteratorsAndComparators/Exercise2/04-Froggy/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exer_04.Froggy
+{
+    public class Cat
+    {
+        public string cats;
+        private string age;
+
+        public Cat()
+        {
+            this.cats = "Jony";
+            this.age = "8";
+        }
+
+        public string NametoVe { get; set; }
+    }
+}
---
+++ C#-Advanced/07-IteratorsAndComparators/Exercise2/04-Froggy/Program.cs
@@ -0,0 +1,27 @@
+﻿using Exer_04.Froggy;
+using System;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var lake = new Lake(inputNumbers);
+            Cat cat = new Cat();
+
+            Console.WriteLine(lake.stones[0]);
+            Console.WriteLine(lake.stones[1]);
+            Console.WriteLine(lake.stones[2]);
+            Console.WriteLine(lake.stones[3]);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
