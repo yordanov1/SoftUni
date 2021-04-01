ffa4a0766115079f04ed5b271cbc92046f40f7b6
yordanov1 <yordanov8801@gmail.com>
2021-04-01 12:51:46 +0300

    Add

---
+++ C#-Advanced/08-Streams/Lab/06-FolderSize/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_06.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = Console.ReadLine();
+
+            var files = Directory.GetFiles(folderPath);
+
+            long fileSizes = 0;
+
+            foreach (var filePath in files)
+            {
+                FileInfo file = new FileInfo(filePath);
+
+                Console.WriteLine(file.FullName);
+                Console.WriteLine(file.Length);
+                Console.WriteLine(file.Extension);
+
+                fileSizes += file.Length;
+            }
+
+            Console.WriteLine(fileSizes / 1024.0);
+        }
+    }
+}
