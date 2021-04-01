84cd7a917b14f54d0cf3ef05224f50e3e9b2c1b4
yordanov1 <yordanov8801@gmail.com>
2021-04-01 12:50:25 +0300

    Add

---
+++ C#-Advanced/08-Streams/Lab/01-OddLines/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"C:\Users\Yordanov\source\repos\Streams\Lab_01.OddLines\input.txt"))
+            {
+                string currentRow = reader.ReadLine();
+                int row = 0;
+
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (currentRow != null)
+                    {
+                        if (row % 2 == 1)
+                        {
+                            writer.WriteLine(currentRow);
+                        }
+
+                        currentRow = reader.ReadLine();
+                        row++;
+                    }
+                }                                
+            }
+        }
+    }
+}
---
+++ C#-Advanced/08-Streams/Lab/02-LineNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int row = 1;
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{row}. {line}");
+                        row++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
