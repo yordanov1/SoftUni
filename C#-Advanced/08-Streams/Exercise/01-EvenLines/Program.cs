2d1c986b6593b5443073c304e7dcea5d9b06652d
yordanov1 <yordanov8801@gmail.com>
2021-04-06 21:13:20 +0300

    Add

---
+++ C#-Advanced/08-Streams/Exercise/01-EvenLines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exer_01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "../../../text.txt";
+
+            int count = 0;
+
+            using (var reader = new StreamReader(filePath))
+            {
+                string currentLine = reader.ReadLine();
+
+                while (currentLine != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        string newLine = currentLine.Replace("-", "@").Replace(",", "@")
+                                                    .Replace(".", "@").Replace("!", "@")
+                                                    .Replace("?", "@");
+
+                        var reversedLine = newLine.Split().Reverse().ToArray();
+
+                        Console.WriteLine(string.Join(" ", reversedLine));
+                    }
+
+                    count++;
+
+                    currentLine = reader.ReadLine();
+                }
+            }
+        }     
+    }
+}
---
+++ C#-Advanced/08-Streams/Exercise/02-LineNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exer_02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textDirectory = "../../../text.txt";
+            string returnDirectory = "../../../return.txt";
+
+            var textLines = File.ReadAllLines(textDirectory);
+
+            int lineCounter = 1;
+
+            foreach (var line in textLines)
+            {
+                int lettersCount = line.Count(char.IsLetter);
+                int marksCount = line.Count(char.IsPunctuation);
+
+                File.AppendAllText(returnDirectory, $"Line {lineCounter}: {line} ({lettersCount})({marksCount}){Environment.NewLine}");
+
+                lineCounter++;
+            }
+        }
+    }
+}
