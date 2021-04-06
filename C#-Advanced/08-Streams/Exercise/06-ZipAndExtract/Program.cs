e079fc3cbf71dd25bed1895e6c0398ddd9766313
yordanov1 <yordanov8801@gmail.com>
2021-04-06 21:15:29 +0300

    Add

---
+++ C#-Advanced/08-Streams/Exercise/06-ZipAndExtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Exer_06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipFile = "../../../result.zip";
+            string picPath = "../../../copyMe.png";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(picPath, Path.GetFileName(picPath));
+            }
+
+            ZipFile.ExtractToDirectory("../../../result.zip", Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+            // -> картинката е в директорията на прохграмата.
+            // -> прави zip фаила в директориата на програмата.
+            // -> разархивира zip фаила на Декстопа.
+        }
+    }
+}
