3df03d628782a68372e7c57bec3fe0be4c7dddc8
yordanov1 <yordanov8801@gmail.com>
2021-04-06 21:14:17 +0300

    Add

---
+++ C#-Advanced/08-Streams/Exercise/03-WordCount/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Exer_03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textDirectory = "../../../text.txt";
+            string wordsDirectory = "../../../words.txt";
+
+            var textLines = File.ReadAllLines(textDirectory);
+            var words = File.ReadAllLines(wordsDirectory);
+
+            var wordsCount = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string lowercaseWord = word.ToLower();
+
+                if (!wordsCount.ContainsKey(lowercaseWord))
+                {
+                    wordsCount.Add(lowercaseWord, 0);
+                }
+            }
+
+            foreach (var line in textLines)
+            {
+                var currentLine = line.ToLower()
+                                      .Split(new char[] { ' ', '-', ',', '!', '?', '.', '\'' });
+
+                foreach (var currentWord in currentLine)
+                {
+                    if (wordsCount.ContainsKey(currentWord))
+                    {
+                        wordsCount[currentWord]++;
+                    }
+                }
+            }
+
+            string actualResultPath = "../../../actualResult.txt";
+            string expectedResultPath = "../../../expectedResult.txt";
+
+            foreach (var kvp in wordsCount)
+            {
+                File.AppendAllText(actualResultPath, $"{kvp.Key} - {kvp.Value}{Environment.NewLine}");
+            }
+
+            foreach (var kvp in wordsCount.OrderByDescending(x => x.Value))
+            {
+                File.AppendAllText(expectedResultPath, $"{kvp.Key} - {kvp.Value}{Environment.NewLine}");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/08-Streams/Exercise/04-CopyBinaryFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Exer_04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string picDirectory = "../../../copyMe.png";
+            string picCopyDirectory = "../../../copyMe-new.png";
+
+            using (var reader = new FileStream(picDirectory, FileMode.Open))
+            {
+                using (var writer = new FileStream(picCopyDirectory, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        var buffer = new byte[4096];
+
+                        int size = reader.Read(buffer, 0, buffer.Length);
+
+                        if (size == 0)
+                        {
+                            break;
+                        }
+
+                        writer.Write(buffer, 0, size);
+                    }
+                }
+            }
+        }
+    }
+}
