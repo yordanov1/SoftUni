4b8b8d9d6f96a1299c7445d53e2b6b551fe96121
yordanov1 <yordanov8801@gmail.com>
2021-04-07 15:46:38 +0300

    Add

---
+++ C#-OOP/01-Inheritance/Lab/02-MultipleInheritance/Puppy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Puppy : Dog
+    {
+
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+
+    }
+}
