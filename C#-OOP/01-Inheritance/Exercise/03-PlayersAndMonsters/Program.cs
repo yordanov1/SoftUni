b4c6161f6fbc051a220e8d824d0eecfdf091c402
yordanov1 <yordanov8801@gmail.com>
2021-04-13 11:43:41 +0300

    Add

---
+++ C#-OOP/01-Inheritance/Exercise/03-PlayersAndMonsters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero("Jony", 99);
+
+            Console.WriteLine(hero);
+        }
+    }
+}
