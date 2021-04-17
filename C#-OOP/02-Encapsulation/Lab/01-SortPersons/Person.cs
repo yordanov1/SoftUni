1c3ffcaf6eb2fdd65cd120df787ce2e84b021e95
yordanov1 <yordanov8801@gmail.com>
2021-04-17 13:01:00 +0300

    Add

---
+++ C#-OOP/02-Encapsulation/Lab/01-SortPersons/Person.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }       
+        
+        public string FirstName
+        {
+            get 
+            {
+                return this.firstName;
+            } 
+            private set 
+            {
+                this.firstName = value; 
+            } 
+        }
+        public string LastName 
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                this.lastName = value;
+            } 
+        }
+        public int Age 
+        {
+            get 
+            {
+                return age;
+            }
+            private set
+            {
+                this.age = value;
+            } 
+        }
+        
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
