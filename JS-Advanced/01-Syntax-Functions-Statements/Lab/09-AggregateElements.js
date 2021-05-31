d7f8406333e29b8367710a7657dfc1e1b7c37e14
Yordan Yordanov <76442068+yordanov1@users.noreply.github.com>
2021-05-31 12:22:54 +0300

    Update 09-AggregateElements.js

--- JS-Advanced/01-Syntax-Functions-Statements/Lab/09-AggregateElements.js
+++ JS-Advanced/01-Syntax-Functions-Statements/Lab/09-AggregateElements.js
@@ -1,9 +1,9 @@
 function solve(elements) {
-
     
     let sum = elements.reduce((a, b) => a + b);
     let inverseValues = elements.reduce((a, b) => a + (1 / b), 0);
     let join = elements.reduce((a,b) => `${a}${b}`, '');
+    
     console.log(sum);
     console.log(inverseValues);
     console.log(join);
