480e361c70b703435e035e998a62b0621c25c915
yordanov1 <yordanov8801@gmail.com>
2021-06-13 23:53:12 +0300

    Add

---
+++ JS-Advanced/02-Arrays-Nested-Arrays/Lab/01-EvenPositionElement.js
@@ -0,0 +1,15 @@
+function even(arr) {
+
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++)
+        if (i % 2 === 0) {
+            newArr.push(arr[i]);
+        }
+
+
+        return newArr.join(' ');
+}
+
+
+console.log(even(['20', '30', '40', '50', '60']))
