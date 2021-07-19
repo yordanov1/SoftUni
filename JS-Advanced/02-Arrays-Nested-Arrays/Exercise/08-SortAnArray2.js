be0846c73cfdd737b19589eda570aa4e0fa53e52
yordanov1 <yordanov8801@gmail.com>
2021-07-19 21:07:29 +0300

    Add

---
+++ JS-Advanced/02-Arrays-Nested-Arrays/Exercise/08-SortAnArray2.js
@@ -0,0 +1,22 @@
+function solve(arr){
+
+    return arr.sort((a, b) => {
+
+    if(a.length === b.length){
+
+        return a.localeCompare(b);        
+    }
+    
+return a.length - b.length;
+
+}).join('\n');
+
+};
+
+console.log(solve(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+
+));
