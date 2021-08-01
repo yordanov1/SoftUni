7d8b14db5822b1f1dc2ff9acb918c0664e3d6a20
yordanov1 <yordanov8801@gmail.com>
2021-08-01 17:46:08 +0300

    Add

---
+++ JS-Advanced/03-Objects-And-Composition/Exercise/02-ConstructionCrew.js
@@ -0,0 +1,12 @@
+function modify(worker) {
+
+    if (worker.dizziness) {
+
+        worker.levelOfHydrated += worker.weight * 0.1 * worker.experience;
+        worker.dizziness = false;
+    }
+
+    return worker;
+}
+
+console.log(modify({ weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true }));
