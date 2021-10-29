27176f68e245162b29db6f2c2c51e639301ae161
yordanov1 <yordanov8801@gmail.com>
2021-10-29 09:01:09 +0300

    Add

---
+++ C#-DB/03-Table-Relations/Exercises/01.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Passports
+(
+	PassportID INT PRIMARY KEY IDENTITY(101, 1),
+	PassportNumber CHAR(8)
+)
+
+
+CREATE TABLE Persons
+(
+	PersonID INT PRIMARY KEY IDENTITY,
+	FirstName VARCHAR(50),
+	Salary DECIMAL(15,2),
+	PassportID INT UNIQUE REFERENCES Passports(PassportID)
+)
+
+INSERT INTO Passports VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO Persons VALUES
+('Roberto', 43300.00, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101)
