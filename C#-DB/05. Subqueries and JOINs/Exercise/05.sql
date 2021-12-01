58e8ecfab2b547a48c06b4e9cec8bb3594969fb6
yordanov1 <yordanov8801@gmail.com>
2021-12-01 11:30:35 +0200

    Add

---
+++ C#-DB/05. Subqueries and JOINs/Exercise/05.sql
@@ -0,0 +1,6 @@
+SELECT TOP(3) e.EmployeeID, e.FirstName 
+	FROM Employees AS e
+	LEFT JOIN EmployeesProjects AS ep ON ep.EmployeeID = e.EmployeeID
+
+WHERE ep.ProjectID IS NULL
+ORDER BY  e.EmployeeID
