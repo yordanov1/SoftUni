620da0bf6ece83ac93d19c5ac04bca84e3f88b86
yordanov1 <yordanov8801@gmail.com>
2021-12-28 23:35:38 +0200

    Add

---
+++ C#-DB/C#-Entity-Framework-Core/04-Entity Relations/Problem-2/P02-FootballBetting/P02-FootballBetting/Program.cs
@@ -0,0 +1,16 @@
+﻿using P03_FootballBetting.Data;
+using System;
+
+namespace P03_FootballBetting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new FootballBettingContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
