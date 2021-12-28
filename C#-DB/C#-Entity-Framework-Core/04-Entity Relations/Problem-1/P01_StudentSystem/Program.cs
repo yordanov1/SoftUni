f8f4aee896e62d06b1da7d8b244cebad62bfbbd6
yordanov1 <yordanov8801@gmail.com>
2021-12-28 23:32:07 +0200

    Add

---
+++ C#-DB/C#-Entity-Framework-Core/04-Entity Relations/Problem-1/P01_StudentSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var context = new StudentSystemContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+    }
+}
