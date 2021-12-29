3d8e58fc354b3a1c48d638a2f85f0a41560029c4
yordanov1 <yordanov8801@gmail.com>
2021-12-29 17:34:33 +0200

    Add

---
+++ C#-DB/C#-Entity-Framework-Core/09-XML-Processing/Problems/CarDealer/DTO/Output/CarOutputModelDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Output
+{
+    [XmlType("car")]
+    public class CarOutputModelDto
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+
+        [XmlElement("travelled-distance")]
+        public long TravelledDistance { get; set; }
+    }
+}
