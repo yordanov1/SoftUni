f616ef3d17d7aec89d261f72fcf56a9812d4d2ac
yordanov1 <yordanov8801@gmail.com>
2021-12-29 18:02:04 +0200

    Add

---
+++ C#-DB/C#-Entity-Framework-Core/09-XML-Processing/Problems-1/CarDealer/DTO/Input/SuplierInputModelDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.Input
+{
+    [XmlType("Supplier")]
+    public class SuplierInputModelDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
