96f97c05474a1be6d10c7bc0ea279cb344aaaf14
yordanov1 <yordanov8801@gmail.com>
2022-07-01 19:37:36 +0300

    Recursion and backtracking 03.Generating01Vectors

---
+++ C#-Algorithms/02.Recursion and Backtracking/03.Generating01Vectors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _03.Generating01Vectors
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var arr = new int[n];
+
+            Gen01(arr, 0);
+        }
+
+        private static void Gen01(int[] arr, int idx)
+        {
+            if (idx >= arr.Length)
+            {
+                Console.WriteLine(String.Join(String.Empty, arr));
+                return;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                arr[idx] = i;
+
+                Gen01(arr, idx + 1);
+            }
+        }
+    }
+}
