2f1106b1d4baae9e30bb938fdb27da927c12e136
yordanov1 <yordanov8801@gmail.com>
2022-07-05 13:59:30 +0300

    RecursiveFactorial

---
+++ C#-Algorithms/02.Recursion and Backtracking/04.RecursiveFactorial/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.RecursiveFactorial
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SumFactorial(number));
+        }
+
+        public static int SumFactorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            if (number == 1)
+            {
+                return 1;
+            }
+
+            return number * SumFactorial(number - 1);
+        }
+    }
+}
