9645f73b1a4a93c668cc105344626fc51eb2eec0
yordanov1 <yordanov8801@gmail.com>
2022-07-15 18:56:46 +0300

    Fibonacci

---
+++ C#-Algorithms/02.Recursion and Backtracking/07.RecursiveFibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.RecursiveFibonacci
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalcFib(input)); // 89
+            //Console.WriteLine(CalcFib(50));
+        }
+
+        static long CalcFib(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            return CalcFib(number - 1) + CalcFib(number - 2);
+        }
+
+    }
+}
