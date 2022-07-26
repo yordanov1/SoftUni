01c12d459cffd370d10c28ab405394f360765d70
yordanov1 <yordanov8801@gmail.com>
2022-07-26 09:37:49 +0300

    Permutations

---
+++ C#-Algorithms/03.Combinatorial Problems/01.Permutation withaut Repetitions Easy/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _01.Permutation_withaut_Repetitions_Easy
+{
+    using System;
+
+    internal class Program
+    {
+        private static string[] elements;
+        private static string[] permutations;
+        private static bool[] used;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            permutations = new string[elements.Length];
+            used = new bool[elements.Length];
+
+
+            Permute(0);
+        }
+
+        private static void Permute(int idx)
+        {
+            if (idx >= permutations.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    permutations[idx] = elements[i];
+                    Permute(idx + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
+    
