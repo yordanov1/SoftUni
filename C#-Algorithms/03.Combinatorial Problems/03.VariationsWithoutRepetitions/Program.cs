718dcb50371f8561d063c5829ae1fe4d5917fe14
yordanov1 <yordanov8801@gmail.com>
2022-08-11 22:32:33 +0300

    Add problem

---
+++ C#-Algorithms/03.Combinatorial Problems/03.VariationsWithoutRepetitions/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _03.VariationsWithoutRepetitions
+{
+    using System;
+
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+        private static bool[] used;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+            used = new bool[elements.Length];
+
+            Variations(0);
+        }
+
+        private static void Variations(int idx)
+        {
+            if (idx >= variations.Length)
+            {
+                Console.WriteLine(String.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    variations[idx] = elements[i];
+                    Variations(idx + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
