a3504e979e292f98167d984b3767ab1ba9fa2fb8
yordanov1 <yordanov8801@gmail.com>
2022-08-27 11:01:52 +0300

    Add Variations With Repetition

---
+++ C#-Algorithms/03.Combinatorial Problems/04.VariationsWithRepetition/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _04.VariationsWithRepetition
+{
+    using System;
+
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+
+            Variations(0);
+        }
+
+        private static void Variations(int idx)
+        {
+            if (idx >= variations.Length)
+            {
+                Console.WriteLine(String.Join(" ", variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                    variations[idx] = elements[i];
+                    Variations(idx + 1);                
+            }
+        }
+    }
+}
