9d3672d81bfe8cf74b077f00d9cd3fb3f3a81eb0
yordanov1 <yordanov8801@gmail.com>
2022-08-27 11:03:23 +0300

    Add

---
+++ C#-Algorithms/03.Combinatorial Problems/06.CombinationsWithRepetition/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _06.CombinationsWithRepetition
+{
+    using System;
+
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            GenCombinations(0, 0);
+        }
+
+        private static void GenCombinations(int idx, int elementsStartIdx)
+        {
+            if (idx >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementsStartIdx; i < elements.Length; i++)
+            {
+                combinations[idx] = elements[i];
+                GenCombinations(idx + 1, i);
+            }
+        }
+    }
+}
