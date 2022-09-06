49530e23a04ce334eced4b4fe9b855bf6692cbd4
yordanov1 <yordanov8801@gmail.com>
2022-09-06 22:53:08 +0300

    Add

---
+++ C#-Algorithms/04.ExerRecursionCombinatorialAlgorithms/01.ReverseArray/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _01.ReverseArray
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elements = Console.ReadLine().Split();
+
+            ReverseArray(elements, 0);
+
+            Console.WriteLine(String.Join(" ", elements));
+        }
+
+        private static void ReverseArray(string[] elements, int idx)
+        {
+            if (idx == elements.Length / 2)
+            {
+                return;
+            }
+
+            var temp = elements[idx];
+            elements[idx] = elements[elements.Length - idx - 1];
+            elements[elements.Length - idx - 1] = temp;
+
+            ReverseArray(elements, idx + 1);
+        }
+    }
+}
