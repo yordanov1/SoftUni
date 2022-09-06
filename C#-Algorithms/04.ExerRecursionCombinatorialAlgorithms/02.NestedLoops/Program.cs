e127128478c199ad63e850721bac57cadbb1ab40
yordanov1 <yordanov8801@gmail.com>
2022-09-06 22:53:53 +0300

    Add Algorithms Problem

---
+++ C#-Algorithms/04.ExerRecursionCombinatorialAlgorithms/02.NestedLoops/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.NestedLoops
+{
+    using System;
+
+    internal class Program
+    {
+        private static int[] elements;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            elements = new int[n];
+
+            GenVectors(0);
+
+        }
+
+        private static void GenVectors(int idx)
+        {
+            if (idx >= elements.Length)
+            {
+                Console.WriteLine(String.Join(" ", elements));
+                return;
+            }
+
+            for (int i = 1; i <= elements.Length; i++)
+            {
+                elements[idx] = i;
+                GenVectors(idx + 1);
+            }
+        }
+    }
+}
