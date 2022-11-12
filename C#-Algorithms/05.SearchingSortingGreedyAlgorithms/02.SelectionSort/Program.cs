d5ba6ef91dde4f09b003f6a5f1bebef7cb51bf3e
yordanov1 <yordanov8801@gmail.com>
2022-11-12 19:59:00 +0200

    Add 02.SelectionSort

---
+++ C#-Algorithms/05.SearchingSortingGreedyAlgorithms/02.SelectionSort/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _02.SelectionSort
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SelectionSort(numbers);
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void SelectionSort(int[] numbers)
+        {
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var min = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(numbers, i, min);
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+
+        // Not Stable
+    }
+}
