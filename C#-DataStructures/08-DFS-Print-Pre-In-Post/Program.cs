7bcdb3aa11ec1ae4f7cd648c3bf55492c5c338f5
yordanov1 <yordanov8801@gmail.com>
2022-12-08 13:37:50 +0200

    Print DFS

---
+++ C#-DataStructures/08-DFS-Print-Pre-In-Post/Program.cs
@@ -0,0 +1,31 @@
+﻿using DataStructures;
+
+
+
+//          1
+//      5       7
+//    2   3   9  11
+
+
+
+Node<int> root = new Node<int>(1,
+    new Node<int>(5,
+         new Node<int>(2),
+         new Node<int>(3)),
+    new Node<int>(7,
+         new Node<int>(9),
+         new Node<int>(11))
+    ); 
+
+BinaryTree<int> tree = new BinaryTree<int>(root);
+
+Console.WriteLine(tree.DFSPreOrder(tree.Root, 0));
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine(tree.DFSInOrder(tree.Root, 0));
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine(tree.DFSPostOrder(tree.Root, 0));
+
