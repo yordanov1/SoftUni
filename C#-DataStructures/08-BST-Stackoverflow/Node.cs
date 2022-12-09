7e56eece41568693516a35576c498301d069d317
yordanov1 <yordanov8801@gmail.com>
2022-12-09 15:06:05 +0200

    BST-Stackoverflow

---
+++ C#-DataStructures/08-BST-Stackoverflow/Node.cs
@@ -0,0 +1,18 @@
+﻿namespace TestBSTVE
+{
+    public class Node
+    {
+        public int value;
+
+        public Node left;
+        public Node right;
+
+
+        public Node(int input)
+        {
+            value = input;
+            left =  null;
+            right = null;
+        }
+    }      
+}
---
+++ C#-DataStructures/08-BST-Stackoverflow/Program.cs
@@ -0,0 +1,43 @@
+﻿using TestBSTVE;
+
+
+
+BinaryTree tree = new BinaryTree();
+
+List<int> arr = new List<int>();
+
+
+for (int i = 0; i < 25; i += 2)
+{
+    arr.Add(i);
+}
+
+
+int n = arr.Count;
+
+Node root = null;
+
+
+root = tree.sortedArrayToBST(arr, 0, n - 1);
+
+
+Console.WriteLine(DFSInOrder(root, 0));
+
+
+static string DFSInOrder(Node node, int indent)
+{
+    string result = "";
+    if (node.left != null)
+    {
+        result += DFSInOrder(node.left, indent + 3);
+    }
+
+    result += $"{new string(' ', indent) }{node.value}\n";
+
+    if (node.right != null)
+    {
+        result += DFSInOrder(node.right, indent + 3);
+    }
+
+    return result;
+}
