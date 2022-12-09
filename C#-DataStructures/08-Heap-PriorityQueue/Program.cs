fdb963d934eec7f33a8b8559383b47aa1b8f030b
yordanov1 <yordanov8801@gmail.com>
2022-12-09 15:29:27 +0200

    Heap and PriorityQueue.cs

---
+++ C#-DataStructures/08-Heap-PriorityQueue/Program.cs
@@ -0,0 +1,30 @@
+﻿using MyHeap;
+
+// Heap ---------------------------------------------------------------------->
+var integerHeep = new MyHeap<int>();
+var elements = new List<int>() { 15, 6, 9, 5, 25, 8, 17, 16};
+
+foreach (var el in elements)
+{
+  integerHeep.Add(el);
+}
+
+Console.WriteLine(integerHeep.DFSInOrder(0, 0));
+Console.WriteLine("\n");
+
+
+
+// Queue --------------------------------------------------------------------->
+var queue = new PriorityQueue<int>();
+elements = new List<int>() { 15, 25, 6, 9, 5, 8, 17, 16 };
+
+foreach (var element in elements)
+{
+    queue.Add(element);
+}
+
+while (queue.Size > 0)
+{
+    //Console.WriteLine(queue.DFSInOrder(0, 0));
+    Console.WriteLine($"Max element: {queue.Deqeueue()}");
+}
