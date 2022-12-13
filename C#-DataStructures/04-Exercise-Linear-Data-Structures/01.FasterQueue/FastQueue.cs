c50324a7aa60687026cc71bce9c6cc3040c38852
yordanov1 <yordanov8801@gmail.com>
2022-12-13 11:23:09 +0200

    Exercise-Linear-Data-Structures

---
+++ C#-DataStructures/04-Exercise-Linear-Data-Structures/01.FasterQueue/FastQueue.cs
@@ -0,0 +1,93 @@
+﻿namespace Problem01.FasterQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class FastQueue<T> : IAbstractQueue<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+
+        public int Count { get; private set; }
+
+
+        public bool Contains(T item)
+        {
+            var node = this.head;
+
+            while (node != null)
+            {
+                if (node.Item.Equals(item))
+                {
+                    return true;
+                }
+
+                node = node.Next;
+            }
+
+            return false;
+        }
+
+        public T Dequeue()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var oldHead = this.head;
+
+            this.head = this.head.Next;
+
+            this.Count--;
+
+            return oldHead.Item;
+        }
+
+        public void Enqueue(T item)
+        {
+            var newNode = new Node<T>(item, null);
+
+            if (this.Count == 0)
+            {
+                this.tail = newNode;
+                this.head = this.tail;
+                
+                this.Count++;
+                return;
+            }
+
+            tail.Next = newNode;
+            this.tail = this.tail.Next;
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.head.Item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Item;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
