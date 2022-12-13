34a7711ea87e84fe7cb2eb86b7135a9c4f03d891
yordanov1 <yordanov8801@gmail.com>
2022-12-13 10:55:37 +0200

    Singly Linked list

---
+++ C#-DataStructures/04-Lab-Linear-Data-Structures/02-SinglyLinkedList/MyLinkedList.cs
@@ -0,0 +1,63 @@
+﻿namespace SinglyLinkedList
+{
+    using System;
+
+    public class MyLinkedList<T>
+    {
+
+        public Node<T> Head { get; set; }
+        public Node<T> Last { get; set; }
+        public int Count { get; set; }
+
+
+        public void Add(T element)
+        {
+            Node<T> newHead = new Node<T>(element);
+            newHead.Next = Head;
+
+            if (Head == null)
+            {
+                Last = newHead;
+            }
+
+            Head = newHead;
+            Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            Node<T> newLast = new Node<T>(element);
+
+            if (Last == null)
+            {
+                Last = newLast;
+                Head = newLast;
+            }
+            else
+            {
+                Last.Next = newLast;
+                Last = newLast;
+            }
+
+            Count++;
+        }      
+
+        public Node<T> RemoveHead()
+        {
+            var oldHead = Head;
+            Head = Head.Next;
+
+            if (Head == null)
+            {
+                Last = null;
+            }
+
+            if (Count > 0)
+            {
+                Count--;
+            }
+
+            return oldHead;
+        }
+    }
+}
---
+++ C#-DataStructures/04-Lab-Linear-Data-Structures/02-SinglyLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using SinglyLinkedList;
+
+
+MyLinkedList<int> linkedList = new MyLinkedList<int>();
+
+
+for (int i = 0; i < 10; i++)
+{
+    linkedList.AddLast(i);
+}
+
+
+
+
+var currentNode = linkedList.Head;
+
+
+while (currentNode != null)
+{
+    Console.WriteLine(currentNode.Value);
+    currentNode = currentNode.Next; 
+}
