ea500a7291b03a2266537172bf12544e7f511308
yordanov1 <yordanov8801@gmail.com>
2022-12-13 10:41:16 +0200

    Queue implementation

---
+++ C#-DataStructures/04-Lab-Linear-Data-Structures/03-Queue/MyQueue.cs
@@ -0,0 +1,32 @@
+﻿namespace QueueImplementation
+{
+    using SinglyLinkedList;
+
+    public class MyQueue<T>
+    {
+        private MyLinkedList<T> linkedList;
+
+        public MyQueue()
+        {
+            linkedList = new MyLinkedList<T>();
+        }
+
+
+        public int Count { get { return linkedList.Count; } }
+       
+        public void Enqueue(T element)
+        {
+            linkedList.AddLast(element);
+        }
+
+        public T Peek()
+        {
+            return linkedList.Head.Value;
+        }
+
+        public T Dequeue()
+        {
+            return linkedList.RemoveHead().Value;
+        }
+    }
+}
---
+++ C#-DataStructures/04-Lab-Linear-Data-Structures/03-Queue/Program.cs
@@ -0,0 +1,28 @@
+﻿using QueueImplementation;
+
+//Test Queue 
+
+MyQueue<int> queue = new MyQueue<int>();
+
+for (int i = 0; i <= 10; i++)
+{
+    queue.Enqueue(i);
+}
+
+Console.WriteLine(queue.Peek());
+
+queue.Dequeue();
+queue.Dequeue();
+
+Console.WriteLine(queue.Peek());
+
+queue.Enqueue(99);
+
+Console.WriteLine(queue.Peek());
+
+while (queue.Count > 0)
+{
+    Console.WriteLine(queue.Dequeue());
+}
+
+
