0d1377c40423fec21725d06c7c1328ab2a934ef9
yordanov1 <yordanov8801@gmail.com>
2022-12-13 11:01:46 +0200

    Stack Implementation

---
+++ C#-DataStructures/04-Lab-Linear-Data-Structures/04-Stack/MyStack.cs
@@ -0,0 +1,33 @@
+﻿namespace StackImplementation
+{
+    using SinglyLinkedList;
+
+    public class MyStack<T>
+    {
+
+        private MyLinkedList<T> linkedList;
+
+        public MyStack()
+        {
+            linkedList = new MyLinkedList<T>();
+        }
+
+        public int Count { get { return linkedList.Count; } }
+
+
+        public void Push(T element)
+        {
+            linkedList.Add(element);
+        }
+
+        public T Peek()
+        {
+            return linkedList.Head.Value;
+        }
+
+        public T Pop()
+        {
+            return linkedList.RemoveHead().Value;
+        }
+    }
+}
---
+++ C#-DataStructures/04-Lab-Linear-Data-Structures/04-Stack/Program.cs
@@ -0,0 +1,26 @@
+﻿using StackImplementation;
+
+
+//Test Stack
+
+MyStack<int> stack = new MyStack<int>();
+
+for (int i = 0; i <= 10; i++)
+{
+    stack.Push(i);
+}
+
+Console.WriteLine(stack.Peek());
+
+stack.Push(99);
+Console.WriteLine(stack.Peek());
+
+stack.Pop();
+Console.WriteLine(stack.Peek());
+
+
+while (stack.Count > 0)
+{
+    Console.WriteLine(stack.Pop());
+}
+
