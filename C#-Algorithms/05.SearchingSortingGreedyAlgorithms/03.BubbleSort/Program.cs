b7b76fcb452b864e45a3b6a7d27665aea1a1db6c
yordanov1 <yordanov8801@gmail.com>
2023-01-24 20:48:31 +0200

    Bubble Sort

---
+++ C#-Algorithms/05.SearchingSortingGreedyAlgorithms/03.BubbleSort/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _03.BubbleSort
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            BubbleSort(numbers);
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            var sortedCount = 0;
+            var isSorted = false;
+
+            while (!isSorted)
+            {
+                isSorted = true;    
+
+                for (int j = 1; j < numbers.Length - sortedCount; j++)
+                {
+                    var i = j - 1;
+
+                    if (numbers[i] > numbers[j])
+                    {
+                        Swap(numbers, i, j);
+                        isSorted = false;
+                    }
+                }
+
+                sortedCount += 1;
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
