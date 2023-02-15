a6af349e5ce48bbe552fe2a81e7afdf323001654
yordanov1 <yordanov8801@gmail.com>
2023-02-15 14:45:48 +0200

    BFS-DFS-Lab

---
+++ C#-DataStructures/06-Lab-BFS-DFS/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace Trees_BFS_DFS
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Node<T>
+    {
+        public Node(T value, params Node<T>[] children)
+        {
+            Value = value;
+            Children = children.ToList();
+        }
+        public T Value { get; set; }
+        public List<Node<T>> Children { get; set; }
+
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
